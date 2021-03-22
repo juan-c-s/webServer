348daf7340cd427e6d02dc7e53d5ec9c320d88f7
juan-c-s <juancamilo.salazar2002@gmail.com>
2021-03-22 13:35:32 -0500

    %

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,7 +13,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('coordinates entered incorrectly, Please try again.', body.error)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' outside, but it feels like ' + response.body.current.feelslike + ' and the humidity is ' + response.body.current.humidity)
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' outside, but it feels like ' + response.body.current.feelslike + ' and the humidity is ' + response.body.current.humidity + '%')
         }
     })
 }
